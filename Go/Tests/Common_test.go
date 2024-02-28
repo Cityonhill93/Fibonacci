ab55fa32a9b1b14eedfe5292d66a65d1c531590f
Brandon Matheson <Cityonhill93@gmail.com>
2024-02-27 20:47:43 -0600

    Fix the names of the test methods for common methods

--- Go/Tests/Common_test.go
+++ Go/Tests/Common_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func GetNextValueReturnsCorrectValue(t *testing.T) {
+func TestGetNextValueReturnsCorrectValue(t *testing.T) {
 	var nextValue = Common.GetNextValue(1, 1)
 
 	if nextValue != 2 {
@@ -13,7 +13,7 @@ func GetNextValueReturnsCorrectValue(t *testing.T) {
 	}
 }
 
-func GetPriorValuesReturnsTwoLatestValues(t *testing.T) {
+func TestGetPriorValuesReturnsTwoLatestValues(t *testing.T) {
 	var allValues [6]int = [6]int{0, 1, 1, 2, 3, 5}
 	var valueOne, valueTwo int = Common.GetPriorValues(allValues[:])
 
