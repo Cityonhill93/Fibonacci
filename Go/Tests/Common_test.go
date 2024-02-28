0ce2d670cf9052202bb9113fb020a858712f4313
Brandon Matheson <Cityonhill93@gmail.com>
2024-02-27 20:34:02 -0600

    Add some unit tests for Go (#12)
    * Add tests for common methods
    
    * Bump go version
    
    ---------
    
    Co-authored-by: Brandon Matheson <Cityonhill15@live.com>

---
+++ Go/Tests/Common_test.go
@@ -0,0 +1,23 @@
+package Tests
+
+import (
+	"Fibonacci/Common"
+	"testing"
+)
+
+func GetNextValueReturnsCorrectValue(t *testing.T) {
+	var nextValue = Common.GetNextValue(1, 1)
+
+	if nextValue != 2 {
+		t.Fail()
+	}
+}
+
+func GetPriorValuesReturnsTwoLatestValues(t *testing.T) {
+	var allValues [6]int = [6]int{0, 1, 1, 2, 3, 5}
+	var valueOne, valueTwo int = Common.GetPriorValues(allValues[:])
+
+	if valueOne != 3 || valueTwo != 5 {
+		t.Fail()
+	}
+}
